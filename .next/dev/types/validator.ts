83915a118cf673cffd6bd186a35c83928febdf97
Thiago <thiago.neves7@fatec.sp.gov.br>
2025-11-10 16:32:56 -0300

    Correções para devido apontamento de backend > Frontend

--- .next/dev/types/validator.ts
+++ .next/dev/types/validator.ts
@@ -128,6 +128,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../../app/auth/login/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/auth/login">> = Specific
+  const handler = {} as typeof import("../../../app/auth/login/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 
 
 
